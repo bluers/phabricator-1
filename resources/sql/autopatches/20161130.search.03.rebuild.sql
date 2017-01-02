1a42c39cb9d3cd9ed6733068dcfe94c2cb40328c
huangshaojun <huangshaojun@yidian-inc.com>
2017-01-02 18:31:08 +0800

    fix startup error again

--- resources/sql/autopatches/20161130.search.03.rebuild.sql
+++ resources/sql/autopatches/20161130.search.03.rebuild.sql
@@ -1,2 +1,12 @@
 USE {$NAMESPACE}_search;
-CREATE FULLTEXT INDEX  IF NOT EXISTS `key_corpus` ON `search_documentfield`(`corpus`,`stemmedCorpus`) WITH PARSER NGRAM;
+select if (
+    exists(
+        select distinct index_name from information_schema.statistics
+        where table_schema = '{$NAMESPACE}_search'
+        and table_name = 'search_documentfield' and index_name like 'key_corpus'
+    )
+    ,'select ''index key_corpus exists'' _______;'
+    ,'CREATE FULLTEXT INDEX `key_corpus` ON `search_documentfield`(`corpus`,`stemmedCorpus`) WITH PARSER NGRAM;') into @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
