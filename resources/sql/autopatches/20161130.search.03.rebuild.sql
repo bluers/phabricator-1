d09b2c903f1d815e58dd0a915bc8f912e375050c
huangshaojun <huangshaojun@yidian-inc.com>
2017-01-02 18:12:40 +0800

    fix startup error

--- resources/sql/autopatches/20161130.search.03.rebuild.sql
+++ resources/sql/autopatches/20161130.search.03.rebuild.sql
@@ -1,2 +1,2 @@
 USE {$NAMESPACE}_search;
-CREATE FULLTEXT INDEX `key_corpus` ON `search_documentfield`(`corpus`,`stemmedCorpus`) WITH PARSER NGRAM;
+CREATE FULLTEXT INDEX  IF NOT EXISTS `key_corpus` ON `search_documentfield`(`corpus`,`stemmedCorpus`) WITH PARSER NGRAM;
