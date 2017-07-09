c1f9a7fde82e6b671d6ace2f00b9ded17ef2875e
huangsj <jimmy_huang@live.com>
2017-07-09 15:48:42 +0800

    token.give和token.given都支持按照分数进行查询和评分。

--- src/applications/tokens/conduit/TokenConduitAPIMethod.php
+++ src/applications/tokens/conduit/TokenConduitAPIMethod.php
@@ -38,6 +38,20 @@ abstract class TokenConduitAPIMethod extends ConduitAPIMethod {
       );
     }
 
+    $tokensScoreAverage = 0;
+    $scores = array('like-1' => 5, 'like-2' => 1, 'heart-1' => 5, 'heart-2' => 1, 'medal-1' => 2,
+      'medal-2' => 3, 'medal-3' => 4, 'medal-4' => 0);
+
+
+    foreach ($tokens_given as $token_given) {
+      $token = $token_given;
+      $tokensScoreAverage = $tokensScoreAverage + $scores[substr($token->getTokenPHID(), 10)];
+    }
+    if(count($tokens_given) > 0){
+      $tokensScoreAverage = $tokensScoreAverage*1.0/count($tokens_given);
+      $list["score"] = $tokensScoreAverage;
+    }
+
     return $list;
   }
 
--- src/applications/tokens/conduit/TokenGiveConduitAPIMethod.php
+++ src/applications/tokens/conduit/TokenGiveConduitAPIMethod.php
@@ -29,9 +29,41 @@ final class TokenGiveConduitAPIMethod extends TokenConduitAPIMethod {
       ->setContentSource($content_source);
 
     if ($request->getValue('tokenPHID')) {
+
+      $tokenPHID = $request->getValue('tokenPHID');
+
+      if($tokenPHID >= 0 && $tokenPHID <= 5 ){
+
+        $viewer = $request->getUser();
+
+        $tokens = id(new PhabricatorTokenQuery())
+          ->setViewer($viewer)
+          ->execute();
+        if ($tokens) {
+          /**
+          array('heart-2', pht('Heartbreak')),//1
+          array('medal-1', pht('Orange Medal')),//2
+          array('medal-2', pht('Grey Medal')),//3
+          array('medal-3', pht('Yellow Medal')),//4
+          array('heart-1', pht('Love')),//5
+           *
+           */
+          $scores = array(5 => 'heart-1', 1 => 'heart-2', 2 => 'medal-1',
+            3 => 'medal-2', 4 => 'medal-3', 0 => 'medal-4');
+          $phid_suffix = $scores[intval($tokenPHID)];
+          foreach ($tokens as $token_given) {
+            $token = $token_given;
+            if(strcmp(substr($token->getPHID(), 10), $phid_suffix) == 0){
+              $tokenPHID = $token->getPHID();
+              break;
+            }
+          }
+        }
+      }
+
       $editor->addToken(
         $request->getValue('objectPHID'),
-        $request->getValue('tokenPHID'));
+        $tokenPHID);
     } else {
       $editor->deleteToken($request->getValue('objectPHID'));
     }
